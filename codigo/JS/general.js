b1cd802ef821968052463880d6d2d953ec0f2663
DiegoRS <sibace_sa@hotmail.com>
2020-01-24 14:24:54 -0600

    ya quedo esa mamda -> (navegador responsive de index.html)

--- codigo/JS/general.js
+++ codigo/JS/general.js
@@ -13,6 +13,22 @@ jQuery(document).ready(function strict (){
 });
 // JavaScript navigator responsive
 $(".submenu").click(function(){
-	$(this).children("ul").slideToggle();
-})
+	$(this).children("ul").slideToggle(); 
+	
+	var Grupo = $(this).parent();
+	var posicion = $(this).index();
+	Grupo.children().each(function (index) {
+	 if (index != posicion)
+	  $(this).children("ul").slideUp("slow");
+	
+	});
+   
+   
+  $("ul").click(function(p){ 
+	p.stopPropagation(); 
+   });
+   
+});
+
+
 
