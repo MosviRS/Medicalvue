6aa55df928e6fbc72a0ef86cccf89007652e8f91
DiegoRS <sibace_sa@hotmail.com>
2020-03-10 21:14:27 -0600

    login y registro

---
+++ codigo/verif_login.php
@@ -0,0 +1,69 @@
+<?php
+session_start();
+if(isset($_SESSION['nombre'])){
+    header("location:datos_medicos.html");
+}
+?>
+
+<!doctype html>
+<html lang="en">
+	<head>
+		<title>Check Login and create session</title>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+	</head>
+	<body>
+		<div class="container">
+		
+			<?php
+			// Connection info. file
+			include 'conn.php';	
+			
+			// Connection variables
+			$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+			// Check connection
+			if (!$conn) {
+				die("Connection failed: " . mysqli_connect_error());
+			}
+			if(isset($_POST["loginmedicos"])){
+                if(!empy($_POST['email']) && !empy($_POST['password'])){
+                    $name=mysqli_real_escape_string($conn,$_POST['email']);
+                    $pass=mysqli_real_escape_string($conn,$_POST['password']);
+                    $result = mysqli_query($conn, "SELECT id, nombres, email, passwrd FROM users WHERE email = '$email'");
+                    $row=mysqli_fetch_array($result);
+
+                    $hash = $row['passwrd'];
+
+                    if (password_verify($_POST['password'], $hash)) {	
+				
+                        $_SESSION['loggedin'] = true;
+                        $_SESSION['name'] = $row['nombres'];
+                        $_SESSION['start'] = time();
+                        $_SESSION['expire'] = $_SESSION['start'] + (60 * 60) ;
+
+                        header("location:datos_medicos.html");
+                       	
+                    
+                    } else {
+                        echo "<div class='alert alert-danger mt-4' role='alert'>Email or Password are incorrects!
+                        <p><a href='login.html'><strong>Please try again!</strong></a></p></div>";			
+                    }	
+                   
+                }
+            }
+				
+			?>
+		</div>
+		<!-- Optional JavaScript -->
+		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+	</body>
+</html>
