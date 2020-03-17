5810c08b14acbc8eaabbba1a68f9ac7743814e46
DiegoRS <sibace_sa@hotmail.com>
2020-03-16 21:15:30 -0600

    cookies

--- codigo/verif_login.php
+++ codigo/verif_login.php
@@ -25,51 +25,7 @@ if(isset($_SESSION['nombres'])){
 
 		$userSession= new UserSession();
 		$user = new User();
-			// Connection info. file
-			//include 'conn.php';	
 			
-			// Connection variables
-			//$conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
-
-			// Check connection
-			//if (!$conn) {
-				//die("Connection failed: " . mysqli_connect_error());
-		//	}
-			
-			// data sent from form login.html 
-			//if(!empty($_POST['email']) && !empty($_POST['password'])){
-
-			//$email = $_POST['email']; 
-			//$password = $_POST['password'];
-			
-			// Query sent to database
-			//$result = mysqli_query($conn, "SELECT nombres, apellidos, email, passwrd FROM users WHERE email = '$email'");
-			
-			// Variable $row hold the result of the query
-			//$row = mysqli_fetch_assoc($result);
-			
-			// Variable $hash hold the password hash on database
-			//$hash = $row['passwrd'];
-			
-			/* 
-			password_Verify() function verify if the password entered by the user
-			match the password hash on the database. If everything is OK the session
-			is created for one minute. Change 1 on $_SESSION[start] to 5 for a 5 minutes session.
-			*/
-			
-			//if (password_verify($_POST['password'], $hash)) {	
-				
-				//$_SESSION['loggedin'] = true;
-				//$_SESSION['name'] = $row['nombres'];
-				//$_SESSION['start'] = time();
-				//$_SESSION['expire'] = $_SESSION['start'] + (1 * 60) ;						
-				
-			    //header("location:datos_medicos.php");
-			
-			//} else {
-				//echo "<div class='alert alert-danger mt-4' role='alert'>Email or Password are incorrects!
-				//<p><a href='login.html'><strong>Please try again!</strong></a></p></div>";			
-			//}
 			if(isset($_SESSION['name'])){
 				$user->setUser($userSession->getCurrentUser());
 				header("location:datos_medicos.php");
